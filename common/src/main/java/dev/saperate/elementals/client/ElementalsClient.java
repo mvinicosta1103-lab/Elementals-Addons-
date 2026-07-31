4d7337e407ae8b69a5438caf7f74642ae810c671
Yagami <mvinicosta1103@gmail.com>
2026-07-31 15:32:50 -0300

    Backing to the addon.
    Signed-off-by: Yagami <mvinicosta1103@gmail.com>

--- common/src/main/java/dev/saperate/elementals/client/ElementalsClient.java
+++ common/src/main/java/dev/saperate/elementals/client/ElementalsClient.java
@@ -22,6 +22,7 @@ import dev.saperate.elementals.client.entities.models.water.WaterBladeModel;
 import dev.saperate.elementals.client.entities.water.*;
 import dev.saperate.elementals.client.features.MetalArmorRenderer;
 import dev.saperate.elementals.client.gui.CastTimerHudOverlay;
+import dev.saperate.elementals.client.gui.ChiHudOverlay;
 import dev.saperate.elementals.client.items.GliderItemRenderer;
 import dev.saperate.elementals.client.keys.KeyCycleBending;
 import dev.saperate.elementals.client.keys.KeyToggleBlueFire;
@@ -75,7 +76,8 @@ public class ElementalsClient {
 
         Services.REGISTRY.registerClientOverlay(
                 ResourceLocation.fromNamespaceAndPath(MODID, "cast_timer"), new CastTimerHudOverlay());
-        // ChiBar disabled: no longer registering the Chi HUD overlay.
+        Services.REGISTRY.registerClientOverlay(
+                ResourceLocation.fromNamespaceAndPath(MODID, "chi_hud"), new ChiHudOverlay());
 
         Services.REGISTRY.registerClientParticles();
         Services.REGISTRY.registerClientColorProviders();
